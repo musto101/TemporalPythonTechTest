617e1d9386d27160504a627224a1abd7dd5e1222
U-DESKTOP-4Q8JLEO\jumpi <musto101@googlemail.com>
2022-08-03 19:10:49 +0100

    Removed seconds from timestamps in the preprocessing.r script.

--- preprocessing.r
+++ preprocessing.r
@@ -3,11 +3,11 @@ library(caret)
 
 dat <- read.csv('data/one_day.csv')
 dat$created_at <- as.character(format(as_datetime(dat$created_at),
-                                    format = "%H:%M:%S"))
+                                    format = "%H:%M"))
 dat$timestamp <- as.character(format(as_datetime(dat$timestamp),
-                                     format = "%H:%M:%S"))
+                                     format = "%H:%M"))
 dat$updated_at <- as.character(format(as_datetime(dat$updated_at),
-                                      format = "%H:%M:%S"))
+                                      format = "%H:%M"))
 
 dat$created_from <- NULL # Its only got one value
 
